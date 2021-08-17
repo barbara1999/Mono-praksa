013c2dc596cc3df3814d2141e6d3efd03ae7ed2d
barbara1999 <barbara1999@gmail.com>
2021-08-17 15:41:18 +0200

    adding project2

---
+++ day2/StudentExample/StudentExample/Areas/HelpPage/ModelDescriptions/EnumValueDescription.cs
@@ -0,0 +1,11 @@
+namespace StudentExample.Areas.HelpPage.ModelDescriptions
+{
+    public class EnumValueDescription
+    {
+        public string Documentation { get; set; }
+
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}
---
+++ day2/StudentExample/StudentExample/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace StudentExample.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
